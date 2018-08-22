c23e84fbfae103ccb028d4ae1e167efb5e9bb3b4
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2018-08-22 10:57:32 +0600

    Add files via upload

---
+++ morejava/ArraySum.java
@@ -0,0 +1,33 @@
+package morejava;
+
+import java.util.Scanner;
+
+public class ArraySum {
+
+	static int sum = 0;
+
+	public static void main(String[] args) {
+
+		int p[] = new int[6];
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter 6 numbers : ");
+
+		for (int counter = 0; counter < 6; counter++) {
+
+			p[counter] = input.nextInt();
+
+		}
+
+		for (int counter = 0; counter < 6; counter++) {
+
+			sum = sum + p[counter];
+
+		}
+
+		System.out.println(sum);
+		
+	}
+
+}
