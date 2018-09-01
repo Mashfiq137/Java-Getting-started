f2fd7a63d7d60e2033481de49735beeff692841b
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2018-09-01 20:37:03 +0600

    Add files via upload

---
+++ MOOC.java
@@ -0,0 +1,19 @@
+package basixjava;
+
+public class MOOC {
+	
+	public static void main(String[] args) {
+
+	String greeting="Hi";
+	
+	String name="Mashfiq";
+	
+	String goodbye=",and goodbye";
+	
+	String sentence=greeting+name+goodbye;
+	
+	System.out.println(sentence);
+	
+}
+	
+}
