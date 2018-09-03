56185fd8890d71779b3b6885d3dc1e550e755106
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2018-09-03 22:01:22 +0600

    Add files via upload

---
+++ StringInput.java
@@ -0,0 +1,23 @@
+package basixjava;
+
+import java.util.Scanner;
+
+public class StringInput {
+
+			public static void main(String[] args) {
+				
+						  
+						  System.out.println("What is your name ?");
+						  
+						  Scanner input=new Scanner(System.in);
+						  
+						  String name;
+						  
+						  name=input.nextLine();
+						  
+						  System.out.println("My name is "+name);
+				
+				
+			}
+	
+}
