0398fde0bb1afc7d494beb63aaa019ab07c6a16d
Mashfiq137 <38359466+Mashfiq137@users.noreply.github.com>
2018-11-08 02:00:09 +0600

    Add files via upload

---
+++ morejava/EvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class EvenOdd {
+	
+	public static void main(String[] args) {
+		Scanner var = new Scanner(System.in);
+		
+		System.out.println("Enter a number");
+		int x = var.nextInt();
+		
+		
+		switch(x%2) {
+		case 1 : System.out.println("Number is Odd");break;
+		case 0 : System.out.println("Number is Even");break;
+		default:System.out.println("Wrong Input , Enter integer");
+		}
+		
+	}
+
+	
+
+}
